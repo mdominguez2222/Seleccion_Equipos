51c5312397290c78b73c93256a3b8e8bd625c46e
Jose <al_jllaveroi@iessierradelasvillas.org>
2023-01-16 10:53:29 +0100

    Imagenes terminadas

--- Trabajo_equipos.py
+++ Trabajo_equipos.py
@@ -5,17 +5,12 @@ def main(page: ft.Page):
     page.update()
 
 
-
-
-
-
-
-
-
-
-
-
-
+    atleti = ft.Image(src=f"/Imagenes/atleti-removebg-preview.png", width=100, height=100,fit=ft.ImageFit.CONTAIN)
+    barsa = ft.Image(src=f"/Imagenes/barsa-removebg-preview(1).png", width=100, height=100,fit=ft.ImageFit.CONTAIN)
+    betis = ft.Image(src=f"/Imagenes/betis-removebg-preview.png", width=100, height=100,fit=ft.ImageFit.CONTAIN)
+    psg = ft.Image(src=f"/Imagenes/psg-removebg-preview.png", width=100, height=100,fit=ft.ImageFit.CONTAIN)
+    real_madrid = ft.Image(src=f"/Imagenes/real_madrid-removebg-preview.png", width=100, height=100,fit=ft.ImageFit.CONTAIN)
+    page.add(atleti,barsa, betis, psg, real_madrid)
 
 
 
@@ -26,4 +21,4 @@ def main(page: ft.Page):
 
 
     
-ft.app(target=main)
+ft.app(target=main, assets_dir="Imagenes")
